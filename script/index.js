2a7b7dd2a7703403fddb475e686b8eabc78471d4
HustleHard <Yokoback@yandex.ru>
2022-12-17 14:00:16 +0400

    Правки после ревью

--- script/index.js
+++ script/index.js
@@ -2,40 +2,39 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 
-const title = document.querySelector('.profile__title');
-const subtitle = document.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector('.profile__title');
+const profilesubtitle = document.querySelector('.profile__subtitle');
+let nameTitle = document.querySelector('.popup__input_name');
+let about = document.querySelector('.popup__input_about');
+const save = document.querySelector('.popup__save-button');
+const mainForm = document.form;
+
+editButton.addEventListener('click', popupOpen);
+function popupOpen() {
+    popup.classList.add('popup_opened');
+}
+
+closeButton.addEventListener('click', popupClose);
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
 
 
+popup.addEventListener('click', (event) => {
 
-
-
-
-editButton.addEventListener('click' , (event) => { 
-    event.preventDefault();
-    popup.classList.add('popup__opened');
-
-})
-
-closeButton.addEventListener('click' , (event) => {
-    popup.classList.remove('popup__opened');
-})
-
- popup.addEventListener('click' , (event) => {
-    
-    if (event.target === event.currentTarget){
-        popup.classList.remove('popup__opened')
+    if (event.target === event.currentTarget) {
+        popup.classList.remove('popup_opened')
     };
 })
 
-const save = document.querySelector('.popup__save');
-save.addEventListener('click' , (event) => {
-    let name = document.querySelector('.popup__name').value; 
-    let about = document.querySelector('.popup__about').value;
-    console.log(name);
-    console.log(about);
-    title.innerHTML='';
-    title.insertAdjacentHTML('beforeend', `<h1 class="profile__title">${name}</h1>`);
-    subtitle.innerHTML='';
-    subtitle.insertAdjacentHTML('beforeend', `<h2 class="profile__subtitle">${about}</h2>`);
-})
- 
+
+
+function handleFormSubmit (event) {
+    event.preventDefault();
+   
+    profileTitle.textContent=nameTitle.value;
+    profilesubtitle.textContent=about.value;
+    
+}
+
+save.addEventListener('click', handleFormSubmit);
